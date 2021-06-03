c505404d896947c636c91056a173cfff893d900f
dan <48936611+dansvel@users.noreply.github.com>
2021-06-03 22:12:16 +0700

    separate the plugin for common and module

--- index.cjs
+++ index.cjs
@@ -1,1 +1,33 @@
-module.exports = require('plugin')
+const marked  = require('marked')
+const frontMatter = require('front-matter')
+
+module.exports = function (pluginOptions = {}, withOrigin = false) {
+  return {
+    name: 'vite-import-markdown',
+    config() {
+      marked.setOptions(pluginOptions)
+    },
+    transform(src, id) {
+      if (!id.endsWith('.md')) return null
+
+      const {attributes, body} = frontMatter(src)
+      let result = {
+        ...attributes,
+        body: marked(body),
+      }
+
+      if (withOrigin) {
+        result = {
+          ...result,
+          markdown: body
+        }
+      }
+
+      return {
+        code: `export default ${JSON.stringify(result)}`,
+        map: null
+      }
+
+    }
+  }
+}
--- plugin.js
+++ plugin.js
@@ -1,36 +1,7 @@
 const marked  = require('marked')
 const frontMatter = require('front-matter')
 
-const plugin = function (pluginOptions = {}, withOrigin = false) {
-  return {
-    name: 'vite-import-markdown',
-    config() {
-      marked.setOptions(pluginOptions)
-    },
-    transform(src, id) {
-      if (!id.endsWith('.md')) return null
 
-      const {attributes, body} = frontMatter(src)
-      let result = {
-        ...attributes,
-        body: marked(body),
-      }
-
-      if (withOrigin) {
-        result = {
-          ...result,
-          markdown: body
-        }
-      }
-
-      return {
-        code: `export default ${JSON.stringify(result)}`,
-        map: null
-      }
-
-    }
-  }
-}
 
 export default plugin()
 module.exports = plugin()
